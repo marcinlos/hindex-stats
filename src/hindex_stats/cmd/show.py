c24285b29a2e40ee82bfeec692917ebcd50e4ba0
Marcin Łoś <marcin.los.91@gmail.com>
2024-01-04 18:16:55 +0100

    Update show command to use new query format

--- src/hindex_stats/cmd/show.py
+++ src/hindex_stats/cmd/show.py
@@ -1,20 +1,28 @@
 import itertools
 
 from hindex_stats.utils import take, SkipOnce
+from hindex_stats.data import Query
 import hindex_stats.fetch as fetch
 
 
 def register_parser(subparsers):
     p = subparsers.add_parser("show", help="show dupa")
-    p.add_argument("query", type=str)
+    p.add_argument("name", type=str, help="author name")
+    p.add_argument("affiliation", type=str, help="author affiliation", nargs="?")
     p.add_argument(
         "-n", "--max-results", help="maximum number of results", type=int, default=5
     )
 
 
+def _build_query(args) -> Query:
+    return Query(args.name, args.affiliation)
+
+
 def execute(args):
+    query = _build_query(args)
+
     separator = SkipOnce(print)
-    for author in take(args.max_results, fetch.search(args.query)):
+    for author in take(args.max_results, fetch.search(query)):
         separator()
         print(f"{author.name}")
         print(f"  {author.affiliation}")
