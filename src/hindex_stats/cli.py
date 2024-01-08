5ac4a1432c452d3830898039180012df6357b121
Marcin Łoś <marcin.los.91@gmail.com>
2024-01-08 04:23:45 +0100

    Add printing version information

--- src/hindex_stats/cli.py
+++ src/hindex_stats/cli.py
@@ -24,8 +24,20 @@ def run(args):
     command(args)
 
 
+def package_version():
+    import importlib.metadata as meta
+
+    return meta.version("hindex-stats")
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--version",
+        help="print version information",
+        action="version",
+        version=f"%(prog)s {package_version()}",
+    )
     subparsers = parser.add_subparsers(title="Actions", dest="action")
 
     command_modules = [load_command_module(cmd) for cmd in COMMANDS]
