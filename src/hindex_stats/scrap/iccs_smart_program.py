49eb107725447e8d53d737afd83f31050ffae291
Marcin Łoś <marcin.los.91@gmail.com>
2024-01-09 04:14:11 +0100

    Add parsing ICCS talk information

--- src/hindex_stats/scrap/iccs_smart_program.py
+++ src/hindex_stats/scrap/iccs_smart_program.py
@@ -9,7 +9,23 @@ class AuthorOccurrence:
     link: str
 
 
+@dataclass
+class Talk:
+    title: str
+    authors: list[AuthorOccurrence]
+
+
 def _parse_author_occurrence(tag: Tag) -> AuthorOccurrence:
     name = tag.text.strip()
     link = tag["href"]
     return AuthorOccurrence(name, link)
+
+
+def _parse_talk(tag: Tag) -> Talk:
+    title_tag = tag.find("div", {"class": "title"})
+    title = next(title_tag.stripped_strings)
+
+    author_tags = tag.find_all("a", {"class": "person"})
+    authors = [_parse_author_occurrence(tag) for tag in author_tags]
+
+    return Talk(title, authors)
--- tests/scrap/test_iccs_smart_program.py
+++ tests/scrap/test_iccs_smart_program.py
@@ -24,12 +24,80 @@ def author_occurence_tag():
     return as_tag(text)
 
 
+@pytest.fixture()
+def talk_tag():
+    text = """
+    <tr class="talk">
+    <td class="time">
+    10:40
+    </td>
+    <td>
+    <a name="talk:168617">
+    </a>
+    <div class="authors">
+        <a name="talk:168617">
+        </a>
+        <a class="person"
+        href="https://easychair.org/smart-program/ICCS2021/person1649.html">
+        Jose Monreal Bailey
+        </a>
+        ,
+        <a class="person"
+        href="https://easychair.org/smart-program/ICCS2021/person1650.html">
+        Hadi Tabatabaee Malazi
+        </a>
+        and
+        <a class="person"
+        href="https://easychair.org/smart-program/ICCS2021/person1651.html">
+        Siobhan Clarke
+        </a>
+    </div>
+    <div class="title">
+        Smoothing Speed Variability in Age-Friendly Urban Traffic Management
+        <span style="font-weight:normal;white-space:nowrap">
+        (
+        <a href="https://easychair.org/smart-program/ICCS2021/2021-06-16.html#talk:168617">
+        abstract
+        </a>
+        )
+        </span>
+    </div>
+    </td>
+    </tr>
+    """
+    return as_tag(text)
+
+
 def test_parsing_author_occurence(author_occurence_tag):
     result = scrap._parse_author_occurrence(author_occurence_tag)
 
     expected = scrap.AuthorOccurrence(
         "Marco Viceconti",
         "https://easychair.org/smart-program/ICCS2021/person2096.html",
+    )
+
+    assert result == expected
+
+
+def test_parsing_talk(talk_tag):
+    result = scrap._parse_talk(talk_tag)
+
+    expected = scrap.Talk(
+        title="Smoothing Speed Variability in Age-Friendly Urban Traffic Management",
+        authors=[
+            scrap.AuthorOccurrence(
+                "Jose Monreal Bailey",
+                "https://easychair.org/smart-program/ICCS2021/person1649.html",
+            ),
+            scrap.AuthorOccurrence(
+                "Hadi Tabatabaee Malazi",
+                "https://easychair.org/smart-program/ICCS2021/person1650.html",
+            ),
+            scrap.AuthorOccurrence(
+                "Siobhan Clarke",
+                "https://easychair.org/smart-program/ICCS2021/person1651.html",
+            ),
+        ],
     )
 
     assert result == expected
