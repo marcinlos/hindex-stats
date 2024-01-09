fd6fd687002cba1e06e1e27beddf07f201ef4709
Marcin Łoś <marcin.los.91@gmail.com>
2024-01-10 00:46:51 +0100

    Add utility function filtering out None values

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,41 @@ import pytest
 import hindex_stats.utils as utils
 
 
+def test_without_none_no_nones():
+    """
+    If no None values are present, nothing is removed.
+    """
+    xs = [1, 2, "a", 8, "asdf"]
+    assert list(utils.without_none(xs)) == xs
+
+
+def test_witout_none_all_nones():
+    """
+    If only None values are present, resulting iterable is empty.
+    """
+    xs = [None, None]
+    assert not list(utils.without_none(xs))
+
+
+def test_without_none_multiple():
+    """
+    Present None values are all removed, while non-None elements
+    are preserved.
+    """
+    xs = [0, 1, None, None, 2, 3, None, 4]
+    expected = [0, 1, 2, 3, 4]
+    assert list(utils.without_none(xs)) == expected
+
+
+def test_without_none_falses_are_preserved():
+    """
+    Values that are considered False in a boolean context are not removed.
+    """
+    xs = [0, 1, None, None, 2, 3, None, 4, [], 5, (), ""]
+    expected = [0, 1, 2, 3, 4, [], 5, (), ""]
+    assert list(utils.without_none(xs)) == expected
+
+
 @pytest.fixture()
 def call_counter():
     return CallCounter()
