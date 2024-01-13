2cda3c03af52f38d5c9ef8618f3cb30139e0de73
Marcin Łoś <marcin.los.91@gmail.com>
2024-01-13 03:16:24 +0100

    Make links in ICCS parsing tests relative
    Web page downloaded using the browser contains full URL links. However,
    the page obtained in response to a direct HTTP request has relative
    links. To ensure the tests have realistic data, change the absolute URLs
    to relative ones.

--- tests/scrap/iccs/test_parser.py
+++ tests/scrap/iccs/test_parser.py
@@ -26,7 +26,7 @@ def as_tag(text):
 def author_occurence_tag():
     text = """
     <a class="person"
-    href="https://easychair.org/smart-program/ICCS2021/person2096.html">
+    href="person2096.html">
     Marco Viceconti
     </a>
     """
@@ -47,17 +47,17 @@ def talk_tag():
         <a name="talk:168617">
         </a>
         <a class="person"
-        href="https://easychair.org/smart-program/ICCS2021/person1649.html">
+        href="person1649.html">
         Jose Monreal Bailey
         </a>
         ,
         <a class="person"
-        href="https://easychair.org/smart-program/ICCS2021/person1650.html">
+        href="person1650.html">
         Hadi Tabatabaee Malazi
         </a>
         and
         <a class="person"
-        href="https://easychair.org/smart-program/ICCS2021/person1651.html">
+        href="person1651.html">
         Siobhan Clarke
         </a>
     </div>
@@ -65,7 +65,7 @@ def talk_tag():
         Smoothing Speed Variability in Age-Friendly Urban Traffic Management
         <span style="font-weight:normal;white-space:nowrap">
         (
-        <a href="https://easychair.org/smart-program/ICCS2021/2021-06-16.html#talk:168617">
+        <a href="2021-06-16.html#talk:168617">
         abstract
         </a>
         )
@@ -111,12 +111,7 @@ def program_doc():
 
 def test_parsing_author_occurence(author_occurence_tag):
     result = parser._parse_author_occurrence(author_occurence_tag)
-
-    expected = parser.AuthorOccurrence(
-        "Marco Viceconti",
-        "https://easychair.org/smart-program/ICCS2021/person2096.html",
-    )
-
+    expected = parser.AuthorOccurrence("Marco Viceconti", "person2096.html")
     assert result == expected
 
 
@@ -126,18 +121,9 @@ def test_parsing_talk(talk_tag):
     expected = parser.Talk(
         title="Smoothing Speed Variability in Age-Friendly Urban Traffic Management",
         authors=[
-            parser.AuthorOccurrence(
-                "Jose Monreal Bailey",
-                "https://easychair.org/smart-program/ICCS2021/person1649.html",
-            ),
-            parser.AuthorOccurrence(
-                "Hadi Tabatabaee Malazi",
-                "https://easychair.org/smart-program/ICCS2021/person1650.html",
-            ),
-            parser.AuthorOccurrence(
-                "Siobhan Clarke",
-                "https://easychair.org/smart-program/ICCS2021/person1651.html",
-            ),
+            parser.AuthorOccurrence("Jose Monreal Bailey", "person1649.html"),
+            parser.AuthorOccurrence("Hadi Tabatabaee Malazi", "person1650.html"),
+            parser.AuthorOccurrence("Siobhan Clarke", "person1651.html"),
         ],
     )
 
@@ -175,32 +161,19 @@ def test_parsing_session(session_tag):
                 title="Smoothing Speed Variability in Age-Friendly "
                 "Urban Traffic Management",
                 authors=[
+                    parser.AuthorOccurrence("Jose Monreal Bailey", "person1649.html"),
                     parser.AuthorOccurrence(
-                        "Jose Monreal Bailey",
-                        "https://easychair.org/smart-program/ICCS2021/person1649.html",
+                        "Hadi Tabatabaee Malazi", "person1650.html"
                     ),
-                    parser.AuthorOccurrence(
-                        "Hadi Tabatabaee Malazi",
-                        "https://easychair.org/smart-program/ICCS2021/person1650.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Siobhan Clarke",
-                        "https://easychair.org/smart-program/ICCS2021/person1651.html",
-                    ),
+                    parser.AuthorOccurrence("Siobhan Clarke", "person1651.html"),
                 ],
             ),
             parser.Talk(
                 title="An innovative employment of NetLogo AIDS model in developing "
                 "a new chain coding mechanism for compression",
                 authors=[
-                    parser.AuthorOccurrence(
-                        "Khaldoon Dhou",
-                        "https://easychair.org/smart-program/ICCS2021/person1439.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Christopher Cruzen",
-                        "https://easychair.org/smart-program/ICCS2021/person1440.html",
-                    ),
+                    parser.AuthorOccurrence("Khaldoon Dhou", "person1439.html"),
+                    parser.AuthorOccurrence("Christopher Cruzen", "person1440.html"),
                 ],
             ),
             parser.Talk(
@@ -208,74 +181,32 @@ def test_parsing_session(session_tag):
                 "Investigating the impact of social distancing and "
                 "checkout zone design",
                 authors=[
-                    parser.AuthorOccurrence(
-                        "Tomasz Antczak",
-                        "https://easychair.org/smart-program/ICCS2021/person1301.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Bartosz Skorupa",
-                        "https://easychair.org/smart-program/ICCS2021/person1302.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Mikolaj Szurlej",
-                        "https://easychair.org/smart-program/ICCS2021/person1303.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Rafal Weron",
-                        "https://easychair.org/smart-program/ICCS2021/person1304.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Jacek Zabawa",
-                        "https://easychair.org/smart-program/ICCS2021/person1305.html",
-                    ),
+                    parser.AuthorOccurrence("Tomasz Antczak", "person1301.html"),
+                    parser.AuthorOccurrence("Bartosz Skorupa", "person1302.html"),
+                    parser.AuthorOccurrence("Mikolaj Szurlej", "person1303.html"),
+                    parser.AuthorOccurrence("Rafal Weron", "person1304.html"),
+                    parser.AuthorOccurrence("Jacek Zabawa", "person1305.html"),
                 ],
             ),
             parser.Talk(
                 title="A multi-cell cellular automata model of traffic flow "
                 "with emergency vehicles: effect of a corridor of life",
                 authors=[
-                    parser.AuthorOccurrence(
-                        "Krzysztof Małecki",
-                        "https://easychair.org/smart-program/ICCS2021/person905.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Marek Kamiński",
-                        "https://easychair.org/smart-program/ICCS2021/person906.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Jarosław Wąs",
-                        "https://easychair.org/smart-program/ICCS2021/person907.html",
-                    ),
+                    parser.AuthorOccurrence("Krzysztof Małecki", "person905.html"),
+                    parser.AuthorOccurrence("Marek Kamiński", "person906.html"),
+                    parser.AuthorOccurrence("Jarosław Wąs", "person907.html"),
                 ],
             ),
             parser.Talk(
                 title="HSLF: HTTP Header Sequence based LSH fingerprints "
                 "for Application Traffic Classification",
                 authors=[
-                    parser.AuthorOccurrence(
-                        "Zixian Tang",
-                        "https://easychair.org/smart-program/ICCS2021/person910.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Qiang Wang",
-                        "https://easychair.org/smart-program/ICCS2021/person911.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Wenhao Li",
-                        "https://easychair.org/smart-program/ICCS2021/person807.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Huaifeng Bao",
-                        "https://easychair.org/smart-program/ICCS2021/person808.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Wen Wang",
-                        "https://easychair.org/smart-program/ICCS2021/person912.html",
-                    ),
-                    parser.AuthorOccurrence(
-                        "Feng Liu",
-                        "https://easychair.org/smart-program/ICCS2021/person810.html",
-                    ),
+                    parser.AuthorOccurrence("Zixian Tang", "person910.html"),
+                    parser.AuthorOccurrence("Qiang Wang", "person911.html"),
+                    parser.AuthorOccurrence("Wenhao Li", "person807.html"),
+                    parser.AuthorOccurrence("Huaifeng Bao", "person808.html"),
+                    parser.AuthorOccurrence("Wen Wang", "person912.html"),
+                    parser.AuthorOccurrence("Feng Liu", "person810.html"),
                 ],
             ),
         ],
